49ad680040a9b67d1f8a14ab5397b620a688dc10
Paul Martin <paul@lexal.net>
2026-02-15 21:43:07 -0500

    added gls alias

---
+++ .oh-my-zsh-custom/aliases.zsh
@@ -0,0 +1,7 @@
+# These seem to be unique across operating systems
+alias lla='ls -Al'
+alias mountt='mount | column -t'
+alias colortable='ruby -e "$(curl -fsSL https://raw.githubusercontent.com/gawin/bash-colors-256/master/colors)"'
+
+# macOS only - this is is to work around some OMZ odd behavior and force use of GNU ls
+alias ls='/opt/homebrew/bin/gls --color=tty'
