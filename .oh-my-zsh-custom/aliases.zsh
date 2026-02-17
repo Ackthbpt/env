0559cf85fc20ec0077f35fd575ce667573739701
Paul Martin <paul@lexal.net>
2026-02-16 21:24:23 -0500

    Making the 'ls' alias change in aliases.zsh OS-aware

--- .oh-my-zsh-custom/aliases.zsh
+++ .oh-my-zsh-custom/aliases.zsh
@@ -3,5 +3,10 @@ alias lla='ls -Al'
 alias mountt='mount | column -t'
 alias colortable='ruby -e "$(curl -fsSL https://raw.githubusercontent.com/gawin/bash-colors-256/master/colors)"'
 
-# macOS only - this is is to work around some OMZ odd behavior and force use of GNU ls
-alias ls='/opt/homebrew/bin/gls --color=tty'
+# Set up OS-specific settings
+case `uname` in
+    Darwin)
+		# macOS only - this is is to work around some OMZ odd behavior and force use of GNU ls
+		alias ls='/opt/homebrew/bin/gls --color=tty'
+        ;;
+esac
